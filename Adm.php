f5fdf010fabd349af47d2daf65da3a33409b29e4
beatrizdomiciano22-commits <beatrizdomiciano.22@gmail.com>
2026-05-14 21:27:37 -0300

    Create Adm.php

---
+++ Adm.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Administração</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div class="container">
+
+    <h1>PAINEL ADMIN</h1>
+
+    <button>Adicionar Palavra</button>
+
+    <button>Remover Palavra</button>
+
+    <button>Ver Jogadores</button>
+
+</div>
+
+</body>
+</html>
