57f657fe229e1bdb2103a6697668a2b40b0cf136
beatrizdomiciano22-commits <beatrizdomiciano.22@gmail.com>
2026-05-14 21:28:22 -0300

    Add Login.php for admin login interface

---
+++ Login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Admin</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div class="container">
+
+    <h1>LOGIN ADMIN</h1>
+
+    <form action="admin.php" method="POST">
+
+        <input type="text" placeholder="Usuário">
+
+        <input type="password" placeholder="Senha">
+
+        <button type="submit">ENTRAR</button>
+
+    </form>
+
+</div>
+
+</body>
+</html>
