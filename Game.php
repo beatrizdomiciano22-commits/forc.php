ab52edbba2482f212b47f5c0e1a605422f8b5f7e
beatrizdomiciano22-commits <beatrizdomiciano.22@gmail.com>
2026-05-14 21:30:46 -0300

    Add initial HTML structure for Game.php

---
+++ Game.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Jogar</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div class="container">
+
+    <h1>JOGO DA FORCA</h1>
+
+    <img src="imagens/forca0.png" width="200">
+
+    <h2>_ _ _ _ _</h2>
+
+    <form>
+        <input type="text" maxlength="1" placeholder="Digite uma letra">
+        <button type="submit">Enviar</button>
+    </form>
+
+</div>
+
+</body>
+</html>
