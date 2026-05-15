26ca65e4bac60dd288d7bd9baaf12000a05cec55
beatrizdomiciano22-commits <beatrizdomiciano.22@gmail.com>
2026-05-14 21:29:37 -0300

    Create Ranking.php

---
+++ Ranking.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ranking</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div class="container">
+
+    <h1>RANKING</h1>
+
+    <table>
+        <tr>
+            <th>Posição</th>
+            <th>Jogador</th>
+            <th>Pontos</th>
+        </tr>
+
+        <tr>
+            <td>1</td>
+            <td>João</td>
+            <td>100</td>
+        </tr>
+
+        <tr>
+            <td>2</td>
+            <td>Maria</td>
+            <td>80</td>
+        </tr>
+    </table>
+
+</div>
+
+</body>
+</html>
