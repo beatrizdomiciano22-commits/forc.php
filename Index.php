c0cf2d83a3ce8f8cdd0f7c5a559af25a74ae3ac2
beatrizdomiciano22-commits <beatrizdomiciano.22@gmail.com>
2026-05-14 21:30:13 -0300

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Jogo da Forca</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div class="container">
+    <h1>JOGO DA FORCA</h1>
+
+    <a href="jogo.php">
+        <button>JOGAR</button>
+    </a>
+
+    <a href="ranking.php">
+        <button>RANKING</button>
+    </a>
+
+    <a href="login.php">
+        <button>ADMIN</button>
+    </a>
+</div>
+
+</body>
+</html>
